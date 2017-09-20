d4dbd754257582c4d4e309d0757911a1e0d6477b
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-20 15:41:01 -0500

    refactor to create students and teachers with loops

--- src/com/TheIronYard/Main.java
+++ src/com/TheIronYard/Main.java
@@ -5,67 +5,69 @@ import java.util.*;
 public class Main {
 
 
+    private Set ;
+
     public static void main(String[] args) {
 
         ArrayList<Student> studentList = new ArrayList<>();
+
+
+        for(int i = 0; i < 15; i++){
+            studentList.add(new Student("Pickle", "Rick", i, 1));
+        }
+
         ArrayList<Teacher> teacherList = new ArrayList<>();
 
+        for(int i = 0; i < 4; i++) {
+            teacherList.add(new Teacher("Hjalmer", "Mises", i, 1));
+        }
+
         HashSet<Student> studentSet1 = new HashSet<>();
-        HashSet<Student> studentSet2 = new HashSet<>();
-        HashSet<Student> studentSet3 = new HashSet<>();
-        HashMap<Teacher, Set> studentMap = new HashMap<>();
-
-
-
-        studentList.add(new Student("Steve", "Smith", 11, 1));
-        studentList.add(new Student("Bob", "Smith", 12, 1));
-        studentList.add(new Student("Sally", "Smith", 13, 1));
-        studentList.add(new Student("Arnold", "Smith", 14, 1));
-        studentList.add(new Student("Betty", "Smith", 15, 1));
-        studentList.add(new Student("Jerry", "Smith", 16, 1));
-        studentList.add(new Student("Rick", "Smith", 17, 1));
-        studentList.add(new Student("Morty", "Smith", 18, 1));
-        studentList.add(new Student("Beth", "Smith", 19, 1));
-        studentList.add(new Student("Evan", "Smith", 20, 1));
-        studentList.add(new Student("Jackson", "Smith", 21, 1));
-        studentList.add(new Student("Gordon", "Smith", 22, 1));
-        studentList.add(new Student("Morgan", "Smith", 23, 1));
-        studentList.add(new Student("Felicia", "Smith", 24, 1));
-        studentList.add(new Student("Eric", "Smith", 25, 1));
-        studentList.add(new Student("Steve", "Smith", 26, 1));
-
-        teacherList.add(new Teacher("Ernst", "Bakunin", 101, 1));
-        teacherList.add(new Teacher("Hermann", "Mendel", 101, 1));
-        teacherList.add(new Teacher("Hjalmer", "Mises", 101, 1));
-
-
         studentSet1.add(studentList.get(0));
         studentSet1.add(studentList.get(1));
         studentSet1.add(studentList.get(2));
         studentSet1.add(studentList.get(3));
         studentSet1.add(studentList.get(4));
 
+        HashSet<Student> studentSet2 = new HashSet<>();
         studentSet2.add(studentList.get(5));
         studentSet2.add(studentList.get(6));
         studentSet2.add(studentList.get(7));
         studentSet2.add(studentList.get(8));
         studentSet2.add(studentList.get(9));
 
+        HashSet<Student> studentSet3 = new HashSet<>();
         studentSet3.add(studentList.get(10));
         studentSet3.add(studentList.get(11));
         studentSet3.add(studentList.get(12));
         studentSet3.add(studentList.get(13));
         studentSet3.add(studentList.get(14));
 
-        studentMap.put(teacherList.get(0), studentSet1);
-        studentMap.put(teacherList.get(1), studentSet2);
-        studentMap.put(teacherList.get(2), studentSet3);
+        HashMap<Teacher, Set> teacherMap = new HashMap<>();
 
 
-        Set studentKeys = studentMap.keySet();
-        Set studentValues = studentMap.values();
 
-        for(Key )
+        teacherMap.put(teacherList.get(0), studentSet1);
+        teacherMap.put(teacherList.get(1), studentSet2);
+        teacherMap.put(teacherList.get(2), studentSet3);
+
+
+
+
+
+
+
+
+
+
+        for(Teacher teacher : teacherMap.keySet()) {
+            System.out.println(teacher.getFirstName()+" "+teacher.getLastName());
+        }
+
+        for(Set studentSet: teacherMap.values()) {
+            System.out.println(studentSet);
+        }
+
 
 
 
